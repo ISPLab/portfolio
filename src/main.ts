cb9d002b39802c73d5d3a8ee8cb3ff7c325e9b0b
Andrey Orlov <for_orlov@mail.ru>
2025-03-14 18:14:29 +0300

    fixed

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,13 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
-import { createPinia } from 'pinia'
 import VueLazyload from 'vue-lazyload'
 
-// Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-
-const vuetify = createVuetify()
-
 const app = createApp(App)
+
 app.use(createPinia())
 app.use(router)
-app.use(vuetify)
-app.use(VueLazyload, {
-  // options
-  preLoad: 1.3,
-  error: 'error-image.jpg',
-  loading: 'loading-image.jpg',
-  attempt: 1
-})
+app.use(VueLazyload)
+
 app.mount('#app') 
