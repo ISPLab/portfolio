4e8784b8f0bc4ac1026d1c9abd819f762b03cd47
Andrey Orlov <for_orlov@mail.ru>
2025-03-20 17:53:43 +0300

    add railway

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { FeedbackModule } from './feedback/feedback.module';
+import { HealthController } from './health/health.controller';
 
 @Module({
     imports: [FeedbackModule],
+    controllers: [HealthController]
 })
 export class AppModule {} 
