832ea422637b6567e5f3e77cca5f1b0cb423f37c
Andrey Orlov <for_orlov@mail.ru>
2025-03-22 17:32:57 +0300

    set 3000 port

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,7 +1,7 @@
 export const config = {
     apiUrl: import.meta.env.DOCKER_ENV === 'true'
-        ? 'http://portfolio-backend:3001/api'
+        ? 'http://portfolio-backend:3000/api'
         : import.meta.env.PROD
-            ? 'http://localhost:3001/api'
+            ? 'http://localhost:3000/api'
             : 'http://localhost:3000/api'
 }; 
